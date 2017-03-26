c9b9e430662cb0a5596b1ff1e414a1d1ec9090e2
Izwan Harris <Izwan Harris>
2017-03-27 01:05:48 +0800

    Updated documentation for Lab 2

--- Lab_2/js/main.js
+++ Lab_2/js/main.js
@@ -1,3 +1,4 @@
+//HTTP GET Request
 $(document).ready(function loadXml(){
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function(){
@@ -6,21 +7,24 @@ $(document).ready(function loadXml(){
            loadMenu(this);
         }
     }
+    //Fetches the xml file to be used in jquery
     xhttp.open("GET", "car_listing.xml", true)
     xhttp.send();
 });
 
     
 function loadMenu(xml){
-    var xmlDoc = xml.responseXML;
-    var car = xmlDoc.getElementsByTagName("car")
+    var xmlDoc = xml.responseXML;                    //Assigns the xml document to a variable
+    var car = xmlDoc.getElementsByTagName("car")        //Finds and returns the elements with tag 'car'
+    //Variables to contain the different car property
     var tempModel;
     var tempPrice;
     var tempDesc;
     var tempMile;
     var tempImg;
-    var menuObj;
+    var carObj;
 
+        //Loops according to the size of car[] (How many car tags that were returned) and assign their values to the variables accordingly
         for(var count = 0; count < car.length; count++){
              tempModel = car[count].getElementsByTagName("model")[0].childNodes[0].nodeValue;
              tempPrice = car[count].getElementsByTagName("price")[0].childNodes[0].nodeValue;
@@ -28,7 +32,8 @@ function loadMenu(xml){
              tempMile = car[count].getElementsByTagName("mileage")[0].childNodes[0].nodeValue;
              tempImg = car[count].getElementsByTagName("image")[0].childNodes[0].nodeValue;
              
-             menuObj = {
+             //Make a Car Object to store the attributes of the car
+             carObj = {
                  "model" : tempModel,
                  "price" : tempPrice,
                  "desc" : tempDesc,
@@ -36,20 +41,21 @@ function loadMenu(xml){
                  "img" : tempImg
              };
              
-             displayMenu(menuObj);
-            //console.log(menuObj);
+             displayMenu(carObj);
+            //console.log(carObj);
         }
 }   
 
-function displayMenu(menuObj){
-   // $("#img").append("<img src=" + menuObj.img + " align='left'/>");      //testing image
-    $("#menu").append(
+//Appends the data from the object to the html class 'menu' for display
+function displayMenu(carObj){
+   // $("#img").append("<img src=" + carObj.img + " align='left'/>");      //testing image
+    $("#listing").append(
         "<table>" +
-        "<tr><td> <img src=" + menuObj.img + " class='imageSize' hspace='5' /></td>" + 
-        "<td valign='top'><h4>" + menuObj.model + 
-        "</h4><p class='price'>" + menuObj.price +
-        "<p>" + menuObj.desc + 
-        "<p>Mileage : " + menuObj.mile + "<p></td></tr>" + 
+        "<tr><td> <img src=" + carObj.img + " class='imageSize' hspace='5' /></td>" + 
+        "<td valign='top'><h4>" + carObj.model + 
+        "</h4><p class='price'>" + carObj.price +
+        "<p>" + carObj.desc + 
+        "<p>Mileage : " + carObj.mile + "<p></td></tr>" + 
         "<tr><td> <hr> </td></tr>" +
         "</table>");
 
