6ea74af5cd410610250d53827014410ad199bb4d
Izwan Harris <Izwan Harris>
2017-04-13 10:04:29 +0800

    Added script to front end. Made changes to XMLHTTPRequest to adapt to the new XML file.

--- Lab_3/js/main.js
+++ Lab_3/js/main.js
@@ -8,54 +8,51 @@ $(document).ready(function loadXml(){
         }
     }
     //Fetches the xml file to be used in jquery
-    xhttp.open("GET", "car_listing.xml", true)
+    xhttp.open("GET", "js/Inventory.xml", true)
     xhttp.send();
 });
 
     
 function loadMenu(xml){
-    var xmlDoc = xml.responseXML;                    //Assigns the xml document to a variable
-    var car = xmlDoc.getElementsByTagName("car")        //Finds and returns the elements with tag 'car'
-    //Variables to contain the different car property
-    var tempModel;
-    var tempPrice;
+    var xmlDoc = xml.responseXML;                           //Assigns the xml document to a variable
+    var prod = xmlDoc.getElementsByTagName("product")        //Finds and returns the elements with tag 'product'
+    //Variables to contain the different product property
+    var tempName;
     var tempDesc;
-    var tempMile;
+    var tempPrice;
     var tempImg;
-    var carObj;
+    var prodObj;
 
-        //Loops according to the size of car[] (How many car tags that were returned) and assign their values to the variables accordingly
-        for(var count = 0; count < car.length; count++){
-             tempModel = car[count].getElementsByTagName("model")[0].childNodes[0].nodeValue;
-             tempPrice = car[count].getElementsByTagName("price")[0].childNodes[0].nodeValue;
-             tempDesc = car[count].getElementsByTagName("description")[0].childNodes[0].nodeValue;
-             tempMile = car[count].getElementsByTagName("mileage")[0].childNodes[0].nodeValue;
-             tempImg = car[count].getElementsByTagName("image")[0].childNodes[0].nodeValue;
+        //Loops according to the size of prod[] (How many prod tags that were returned) and assign their values to the variables accordingly
+        for(var count = 0; count < prod.length; count++){
+             tempName = prod[count].getElementsByTagName("name")[0].childNodes[0].nodeValue;
+             tempDesc = prod[count].getElementsByTagName("desc")[0].childNodes[0].nodeValue;
+             tempPrice = prod[count].getElementsByTagName("price")[0].childNodes[0].nodeValue;
+             tempImg = prod[count].getElementsByTagName("img")[0].childNodes[0].nodeValue;
              
-             //Make a Car Object to store the attributes of the car
-             carObj = {
-                 "model" : tempModel,
-                 "price" : tempPrice,
+             //Make a prod Object to store the attributes of the prod
+             prodObj = {
+                 "name" : tempName,
                  "desc" : tempDesc,
-                 "mile" : tempMile,
+                 "price" : tempPrice,
                  "img" : tempImg
              };
              
-             displayMenu(carObj);
-            //console.log(carObj);
+             displayMenu(prodObj);
+            //console.log(prodObj);
         }
 }   
 
 //Appends the data from the object to the html class 'menu' for display
-function displayMenu(carObj){
-   // $("#img").append("<img src=" + carObj.img + " align='left'/>");      //testing image
+function displayMenu(prodObj){
+   // $("#img").append("<img src=" + prodObj.img + " align='left'/>");      //testing image
     $("#listing").append(
         "<table>" +
-        "<tr><td> <img src=" + carObj.img + " class='imageSize' hspace='5' /></td>" + 
-        "<td valign='top'><h4>" + carObj.model + 
-        "</h4><p class='price'>" + carObj.price +
-        "<p>" + carObj.desc + 
-        "<p>Mileage : " + carObj.mile + "<p></td></tr>" + 
+        "<tr><td> <img src=" + prodObj.img + " class='imageSize' hspace='5' /></td>" + 
+        "<td valign='top'><h4>" + prodObj.name + 
+        "</h4><p class='price'>$" + prodObj.price +
+        "<p>" + prodObj.desc + 
+        "<p>Mileage : " + prodObj.mile + "<p></td></tr>" + 
         "<tr><td> <hr> </td></tr>" +
         "</table>");
 
